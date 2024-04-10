a2ecb1a3c03a1ed3f834abd4a60ae1d779ee48da
rushabh45 <120526324+rushabh45@users.noreply.github.com>
2024-04-10 03:14:32 -0400

    Add files via upload

--- source_code/src/App.js
+++ source_code/src/App.js
@@ -1,16 +1,22 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "./pages";
-import SigninPage from "./pages/signin";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/index';
+import SigninPage from './pages/signin';
+import { useState } from 'react';
+import ChatPage from './pages/ChatPage';
 
 function App() {
+  
+
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/signin" component={SigninPage} exact />
-      </Switch>
+     <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/signin" element={<SigninPage />} />
+  <Route path="/chat" element={<ChatPage />} />
+</Routes>
     </Router>
   );
 }
