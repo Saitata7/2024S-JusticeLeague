81003e7a5109a0cdcf93093ea8405a0a51042c64
saitata7 <saitata7@gmail.com>
2024-05-02 19:11:31 -0400

    reload auth issue bug has fixed

--- source_code/src/pages/PostPage.js
+++ source_code/src/pages/PostPage.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NewsPostComponent from '../components/newsPost/NewsPostComponent';
 import NewsDisplayComponent from '../components/Newsdisplay/NewsDisplayComponent';
 import Footer from "../components/footer";
 import { auth } from "../components/firebase/config";
 
 const PostPage = () => {
+  const [authInitialized, setAuthInitialized] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setAuthInitialized(true);
+    });
+
+    // Unsubscribe from auth state changes when component unmounts
+    return () => unsubscribe();
+  }, []); 
+
   return (
     <div>
-      {auth.currentUser && (
-      <NewsPostComponent />
+      {authInitialized && (  
+        auth.currentUser && (
+          <NewsPostComponent />
+        )
       )}
       <NewsDisplayComponent />
       <Footer />
