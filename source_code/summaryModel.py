8b8936eefe2648337084ed3e956b8575d74d1085
manthankale <47059933+manthankale@users.noreply.github.com>
2024-05-07 11:19:22 -0400

    Update summaryModel.py for tokenize and encoder

--- source_code/summaryModel.py
+++ source_code/summaryModel.py
@@ -29,3 +29,25 @@ X = df_main['Text']
 y = df_main['summary']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 42)
+
+#Tokenize input and target summaries
+tokenizer_x = Tokenizer(num_words=1000)
+tokenizer_x.fit_on_texts(X_train)
+X_train_sequences = tokenizer_x.texts_to_sequences(X_train)
+#max_input_len = max(len(seq) for seq in X_train_sequences)
+
+tokenizer_y = Tokenizer(num_words=250)
+tokenizer_y.fit_on_texts(y_train)
+y_train_sequences = tokenizer_y.texts_to_sequences(y_train)
+#max_traget_len = max(len(seq) for seq in y_train_sequences)
+
+X_train_padded = pad_sequences(X_train_sequences, maxlen = 1000, padding = 'post')
+y_train_padded = pad_sequences(y_train_sequences, maxlen = 250, padding = 'post')
+
+
+#encoder
+encoder_input = Input(shape = (1000,))
+encoder_embedding = Embedding(input_dim = 1000, output_dim = 250)(encoder_input)
+encoder_lstm = LSTM(256,return_sequences=True, return_state= True)
+encoder_output, state_h, state_c = encoder_lstm(encoder_embedding)
+encoder_states = [state_h, state_c]
