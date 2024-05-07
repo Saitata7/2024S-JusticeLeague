d880f7e95b715d29891b52db93392c957d2b497a
manthankale <47059933+manthankale@users.noreply.github.com>
2024-05-07 09:00:36 -0400

    Update index.js for summarize

--- source_code/src/components/infoSection/index.js
+++ source_code/src/components/infoSection/index.js
@@ -4,6 +4,7 @@ import "../../App.css";
 import { InfoContainer, InfoWrapper, Subtitle, Title } from "./InfoElements";
 import { ButtonBasic } from "../ButtonElements";
 import GrowingTextarea from "../VerifyTextArea/Growingtextarea";
+import { useState } from "react";
 
 export const InfoSection = ({ id, title, subtitle, text, image, btnText }) => {
   return (
@@ -30,10 +31,38 @@ export const InfoSection = ({ id, title, subtitle, text, image, btnText }) => {
 };
 
 export const InfoSection_upload = ({ id, title, subtitle, text, image, btnText }) => {
-  const myStyle = {
-    marginTop: '6px',
-    
+  const [isSummarizing, setIsSummarizing] = useState(false);
+  const [summary, setSummary] = useState("");
+  const [showImage, setShowImage] = useState(true);
+  const [inputText, setInputText] = useState("");
+
+  const handleSummarize = () => {
+    setIsSummarizing(true);
+    setShowImage(false);
+
+    // Make the API request to the Flask backend
+    fetch("http://127.0.0.1:5000/summarize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data: inputText }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setIsSummarizing(false);
+        setSummary(data.Summary);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsSummarizing(false);
+      });
   };
+
+  const handleInputChange = (value) => {
+    setInputText(value);
+  };
+
   return (
     <>
       <InfoContainer id={id} className="bg-black">
@@ -42,16 +71,22 @@ export const InfoSection_upload = ({ id, title, subtitle, text, image, btnText }
             <div className="col-lg-6 col-sm-12 my-auto">
               <Subtitle>{subtitle}</Subtitle>
               <Title className="text-white mb-5">{title}</Title>
-             <GrowingTextarea />
-              {/* <div style={myStyle}>
-              <ButtonBasic to="" primary="true" dark="true" className="mx-auto" >
-                {btnText}
-              </ButtonBasic>
-              </div> */}
+              <GrowingTextarea onInputChange={handleInputChange} onSummarize={handleSummarize}  />
              
+              
             </div>
-            <div className="col-lg-6 col-sm-12 img-wrap">
-              <img src={image} alt="" className="fit-img"></img>
+            <div className="col-lg-6 col-sm-12">
+              {isSummarizing ? (
+                <div>Loading...</div>
+              ) : showImage ? (
+                <div className="img-wrap">
+                  <img src={image} alt="" className="fit-img" />
+                </div>
+              ) : (
+                <div style={{ maxHeight: "200px", overflowY: "auto", color: "white" }}>
+                  {summary}
+                </div>
+              )}
             </div>
           </div>
         </InfoWrapper>
