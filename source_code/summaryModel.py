9a444655bf659d974cfea3e5f303b9882395ecb5
manthankale <47059933+manthankale@users.noreply.github.com>
2024-05-08 11:14:23 -0400

    Update summaryModel.py done

--- source_code/summaryModel.py
+++ source_code/summaryModel.py
@@ -7,11 +7,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Define the path to the CSV file containing the data
 csv_file_path = '/content/main.csv'
 df_main = pd.read_csv(csv_file_path)
 
-# Read the CSV file into a Pandas DataFrame
 port_stem = PorterStemmer()
 def clean_my_data(text):
     cleaned_text = re.sub(r'[^A-Za-z0-9]+', ' ', text)
@@ -29,6 +27,7 @@ X = df_main['Text']
 y = df_main['summary']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 42)
+
 
 #Tokenize input and target summaries
 tokenizer_x = Tokenizer(num_words=1000)
@@ -52,7 +51,6 @@ encoder_lstm = LSTM(256,return_sequences=True, return_state= True)
 encoder_output, state_h, state_c = encoder_lstm(encoder_embedding)
 encoder_states = [state_h, state_c]
 
-
 #decoder
 
 decoder_input_data = pad_sequences(y_train_sequences, maxlen = 250, padding = 'post')[:,:-1]
@@ -71,3 +69,48 @@ decoder_output = decoder_dense(decoder_combined_context)
 RNN_model = Model([encoder_input,decoder_input], decoder_output)
 RNN_model.compile(optimizer = 'adam', loss= 'sparse_categorical_crossentropy')
 RNN_model.fit([X_train_padded,decoder_input_data], decoder_target_data, batch_size=32, epochs = 10)
+
+
+import joblib
+joblib.dump(RNN_model, 'RNN_model.pkl')
+
+
+# Preprocess test data
+X_test_sequences = tokenizer_x.texts_to_sequences(X_test)
+X_test_padded = pad_sequences(X_test_sequences, maxlen=1000, padding='post')
+
+y_test_sequences = tokenizer_y.texts_to_sequences(y_test)
+decoder_input_test_data = pad_sequences(y_test_sequences, maxlen=250, padding='post')[:, :-1]
+decoder_target_test_data = pad_sequences(y_test_sequences, maxlen=250, padding='post')[:, 1:]
+
+# Generate predictions on test data
+predicted_summaries = RNN_model.predict([X_test_padded, decoder_input_test_data], batch_size=32)
+
+
+import numpy as np
+#decoding predicted summaries from sequences to text
+predicted_summaries_text = []
+for summary_sequence in predicted_summaries:
+    predicted_summary = []
+    for token_index in summary_sequence:
+        predicted_word = tokenizer_y.index_word.get(np.argmax(token_index))
+        if predicted_word == '<end>' or len(predicted_summary) >= 250:
+            break
+        predicted_summary.append(predicted_word)
+    predicted_summary_text = ' '.join(predicted_summary)
+    predicted_summaries_text.append(predicted_summary_text)
+
+#evaluating the predicted summary to actual summary
+
+for i in range(5):
+    print("Input Text:", X_test.iloc[i])
+    print()
+    print("Target Summary:", y_test.iloc[i])
+    print()
+    print("Predicted Summary:", predicted_summaries_text[i])
+    print()
+
+
+
+
+
