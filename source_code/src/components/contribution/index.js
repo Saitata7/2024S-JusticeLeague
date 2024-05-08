f942ca7fc94a172c7dcabf3dd9c100be72bc5333
KavitaK10 <kavitakamtekar04@gmail.com>
2024-05-08 00:05:51 -0400

    Update description for our team in contribution page

--- source_code/src/components/contribution/index.js
+++ source_code/src/components/contribution/index.js
@@ -30,62 +30,57 @@ const Contribution = () => {
           By empowering users with tools to discern truth from falsehood, 
           we contribute to fostering a more informed society.
          </ServicesP>
-         <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+         <ServicesA href="linkedin.com/in/rushabhshingala594551170" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Saurabh} />
           <ServicesH2>Saurabh Chaudhary</ServicesH2>
           
           <ServicesP>
-          AI/ML algorithms enable the differentiation between genuine 
-          and deceptive news through classification.
+          I'm an software engineer with a passion for learning and exploring new technologies especially working on backend.
           </ServicesP>
-          <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+          <ServicesA href="https://www.linkedin.com/in/saurabh-chaudhary-8323982b2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={kavita} />
           <ServicesH2>Kavita Kamtekar</ServicesH2>
           <ServicesP>
-          AI/ML algorithms enable the differentiation between genuine 
-          and deceptive news through classification.
+          I'm an enthusiastic software engineer who thrives on learning about 
+          cutting-edge technologies and diving deep into their exploration.
           </ServicesP>
-          <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+          <ServicesA href="https://www.linkedin.com/in/kavita-kamtekar/" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Maheshwari} />
           <ServicesH2>Maheshwari Vidyadharani</ServicesH2>
           <ServicesP>
-          AI/ML algorithms enable the differentiation between genuine 
-          and deceptive news through classification.
+          I'm an enthusiastic software engineer with a passion for learning and exploring new technologies.
           </ServicesP>
-          <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+          <ServicesA href="https://www.linkedin.com/in/maheswari-vidyadharani-013736253/" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Manthan} />
           <ServicesH2>Manthan Kale</ServicesH2>
           <ServicesP>
-          AI/ML algorithms enable the differentiation between genuine 
-          and deceptive news through classification.
+          I am software Engineer, I enjoy solving difficult problems and give accurate outcomes.
           </ServicesP>
-          <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+          <ServicesA href="https://www.linkedin.com/in/m-pankaj-kale-1a0963217?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Suraj} />
           <ServicesH2>Suraj Salunkhe</ServicesH2>
           <ServicesP>
-          AI/ML algorithms enable the differentiation between genuine 
-          and deceptive news through classification.
+          "I am software Engineer, who is achieving function while avoiding failure.
           </ServicesP>
-          <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+          <ServicesA href="https://www.linkedin.com/in/suraj-salunkhe-8300b8169?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Gayatri} />
           <ServicesH2>Gayatri Kulkarni</ServicesH2>
           <ServicesP>
-          AI/ML algorithms enable the differentiation between genuine 
-          and deceptive news through classification.
+          I am software engineer, I follows my passion to build learn about more technologies.
           </ServicesP>
-          <ServicesA href="https://www.linkedin.com/in/saitata/" target="_blank">LinkedIn</ServicesA>
+          <ServicesA href="https://www.linkedin.com/in/gayatri-kishor-kulkarni-56468a217?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</ServicesA>
         </ServicesCard>
       </ServicesWrapper>
     </ServicesContainer>
