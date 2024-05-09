84636c2240ad4d9d443b08219d497eda9acfb0a7
rushabh45 <120526324+rushabh45@users.noreply.github.com>
2024-05-09 17:25:21 -0400

    Update config.js

--- source_code/src/components/firebase/config.js
+++ source_code/src/components/firebase/config.js
@@ -4,6 +4,14 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   // Your Firebase configuration object goes here
+apiKey: "AIzaSyA9HDUUwoH744uaNP21bO-gjWPGUzvZsss",
+  authDomain: "challenge-30fd1.firebaseapp.com",
+  databaseURL: "https://challenge-30fd1-default-rtdb.firebaseio.com",
+  projectId: "challenge-30fd1",
+  storageBucket: "challenge-30fd1.appspot.com",
+  messagingSenderId: "1007890852867",
+  appId: "1:1007890852867:web:ab6b38ece09c12bf893236",
+  measurementId: "G-TN9TPPNFEV"
 
 };
 
